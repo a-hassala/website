3635373e4779f1ad170009d8dc203633c7463ae9
Anasse <marlow@minet.net>
2014-09-30 19:18:29 +0200

    mise en route de Bootstrap

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  <html>
  <head>
  	<meta charset="utf-8" />
+ 	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
  	<link rel="stylesheet" href="style.css" />
  	<title>HASSALA Anasse WebSite</title>
  </head>
@@ -10,14 +11,21 @@
 
 
  <body>
- 	<div id="navigation_lang">
- 		<table>
- 			<tr><a href="index.php?lang=fr"><img src="Images/fr.png" height="14" width="20" alt="français"/></a></tr>
- 			<tr><span>   </span></tr>
- 			<tr><a href="index.php?lang=en"><img src="Images/uk.jpg" height="14" width="20" alt="english"/></a></tr>
- 			<tr><span>   </span></tr>
- 			<tr><a href="index.php?lang=es"><img src="Images/es.png" height="14" width="20" alt="español"/></a></tr>
- 		</table>
+ 	<div class="container">
+ 		<div class="row">
+ 			<div class="col-lg-push-11 col-lg-2">
+
+ 				<div id="navigation_lang">
+ 					<table>
+ 						<tr><a href="index.php?lang=fr"><img src="Images/fr.png" height="14" width="20" alt="français"/></a></tr>
+ 						<tr><span>   </span></tr>
+ 						<tr><a href="index.php?lang=en"><img src="Images/uk.jpg" height="14" width="20" alt="english"/></a></tr>
+ 						<tr><span>   </span></tr>
+ 						<tr><a href="index.php?lang=es"><img src="Images/es.png" height="14" width="20" alt="español"/></a></tr>
+ 					</table>
+ 				</div>
+ 			</div>
+ 		</div>
  	</div>
 
  	<nav id="navigation">
@@ -28,12 +36,6 @@
  			<tr><a href="cv.php"> <?php echo $cv ?> </a></tr>
  		</table>
  	</nav>
-
- 	<p class="presentation">
-
-
- 	</p>
-
 
  </body>
 
