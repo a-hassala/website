42d776817640a2b20af1bf67d68136e669e10284
Anasse <marlow@minet.net>
2015-03-15 17:03:42 +0100

    ajoute d'une meta description

--- header.php
+++ header.php
@@ -6,16 +6,16 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
+	<meta name="description" content="Site web de Anasse HASSALA, Élève ingénieur en sécurité des systèmes d'information">
 	<meta name="author" content="HASSALA Anasse">
 
-	<title>HASSALA Anasse</title>
+	<title>Anasse HASSALA</title>
 
 	<!-- Bootstrap Core CSS -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Custom CSS -->
-	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> 
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" />
 	<link rel="stylesheet" type="text/css" href="css/resume.css" media="all" />
 	<link href="css/simple-sidebar.css" rel="stylesheet">
 
