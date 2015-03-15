6fe9f5019c5266f1447ade504a551f724f15294b
Anasse <marlow@minet.net>
2015-03-15 16:21:14 +0100

    Correction d'erreur de lien dans un tableau

--- nav-lang.php
+++ nav-lang.php
@@ -1,6 +1,15 @@
 <table>
- 						<tr><a href="?lang=fr"><img src="Images/fr.png" height="14" width="20" alt="français"/></a></tr>
- 						<tr><span>   </span></tr>
- 						<tr><a href="?lang=en"><img src="Images/en.png" height="14" width="20" alt="english"/></a></tr>
- 						<tr><span>   </span></tr>
- 					</table>
+       <tr>
+              <td><a href="?lang=fr"><img src="Images/fr.png" height="14" width="20" alt="français"/></a>
+              </td>
+       </tr>
+
+       <tr><span>   </span></tr>
+       <tr>
+              <td>
+                     <a href="?lang=en"><img src="Images/en.png" height="14" width="20" alt="english"/></a>
+              </td>
+       </tr>
+
+       <tr><span>   </span></tr>
+</table>
