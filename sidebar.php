eded35e036b98c066b3ab987b2452ffb9ad421b7
Anasse <marlow@minet.net>
2015-03-15 16:15:48 +0100

    correction de l'echappement d'un & dans une url

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,11 @@
     				<li class="nav-projects"><a href="projects.php"> <?php echo $projects ; ?></a></li>
     				<li class="nav-cv"><a href="cv.php"> <?php echo $cv ?> </a></li>
     				<li class="nav-contacts"><a href="contacts.php"> <?php echo $contacts ; ?></a></li>
-    				<li class="footer"><a href="https://www.linkedin.com/profile/view?id=364827678&amptrk=nav_responsive_tab_profile_pic" target="_blank"><img src="Images/linkedin.png" alt="in"></a></li>
+    				<li class="footer"><a href="https://www.linkedin.com/profile/view?id=364827678&amp;trk=nav_responsive_tab_profile_pic" target="_blank"><img src="Images/linkedin.png" alt="in"></a></li>
     				<li class="footer"><a href="https://plus.google.com/114603067289499766106/posts?hl=fr" target="_blank"><img src="Images/google.png" alt="G+"></a></li>
     				<li class="footer"><a href="https://twitter.com/AnassHassala" target="_blank"><img src="Images/twitter.png" alt="Twitter"></a></li>
     				<li class="footer"><a href="https://facebook.com/anass.hassala" target="_blank"><img src="Images/facebook.png" alt="f"></a></li>
 
     			</ul>
-    				
+
     		</div>
